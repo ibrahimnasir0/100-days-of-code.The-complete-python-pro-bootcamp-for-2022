1cc1d794357f6058c7381c1b88db9909ed950c12
Ibrahim <m.ibrahimnasir2020@gmail.com>
2022-08-11 04:05:29 +0500

    Day1 first project

---
+++ Day 1/Day 1.py
@@ -0,0 +1,30 @@
+# print("hello"+"ibrahim")
+
+# print("Hello "+input("What is your name"))
+a = input("What is your name ? ")
+print(len(a))
+
+#Changing of data from one to another & vice versa
+#Dont change this
+a = input("a:")
+b = input("b:")
+#Do here
+c=a
+a=b
+b=c
+
+#Dont change this
+print("a = " + a)
+print("b = " + b)
+
+
+
+print("Hi there! Are you confused with ur band name? We are here to help")
+
+city = input("Enter your city you grew up in: \n ")
+
+pet = input("Enter your pet's name: \n ")
+
+final = city + " "+pet
+
+print("Congratulations! Your new band name is "+ final)
