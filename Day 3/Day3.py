b21ccf8c456dece1e8a2e854cac4f989cc52cf43
Ibrahim <m.ibrahimnasir2020@gmail.com>
2022-08-15 22:39:28 +0500

    Treasure Island Project

--- Day 3/Day3.py
+++ Day 3/Day3.py
@@ -1,143 +1,144 @@
-# #if else statement
-# #Nested if and elif statements
-# print("Welcome to the rollercoaster.")
-# height = int(input("what is yout height in cm ? "))
-# bill = 0
-# if height >= 120:
-#     print("You can ride the rollcoaster!")
-#     age = int(input("What is your age? "))
-#     if age <12:
-#         bill = 5
-#         print("Please pay 5$")
-#     elif age>=12 & age <18:
-#         bill = 7
-#         print("Please pay 7$")
-#     else :
-#         bill = 12
-#         print("Please pay 12$")
+#if else statement
+#Nested if and elif statements
+print("Welcome to the rollercoaster.")
+height = int(input("what is yout height in cm ? "))
+bill = 0
+if height >= 120:
+    print("You can ride the rollcoaster!")
+    age = int(input("What is your age? "))
+    if age <12:
+        bill = 5
+        print("Please pay 5$")
+    elif age>=12 & age <18:
+        bill = 7
+        print("Please pay 7$")
+    else :
+        bill = 12
+        print("Please pay 12$")
     
-#     wants_photo = input("Do you want a photo taken? Y or N. ")
-#     if wants_photo == "Y" :
-#         #Add $3 to thier bill
-#         bill = bill + 3
-#     print(f"Your Fina bill is {bill}")
-# else:
-#     print("Sorry! you have to grow taller before you can ride.")
-#
-# # one equal(=) means its assigning
-# # two equal(==) means its checking equality
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y" :
+        #Add $3 to thier bill
+        bill = bill + 3
+    print(f"Your Fina bill is {bill}")
+else:
+    print("Sorry! you have to grow taller before you can ride.")
 
-# #First Challenge
-# #Odd or even
-# number = int(input("Type any number.    "))
-# modulo = number % 2
+# one equal(=) means its assigning
+# two equal(==) means its checking equality
 
-# if modulo == 0:
-#     print("Number is even.")
-# else:
-#     print("Number is odd.")
+#First Challenge
+#Odd or even
+number = int(input("Type any number.    "))
+modulo = number % 2
+
+if modulo == 0:
+    print("Number is even.")
+else:
+    print("Number is odd.")
     
-# #Nested if and elif statements
-# # Second Challenge
-# #BMI Calculator
+#Nested if and elif statements
+# Second Challenge
+#BMI Calculator
 
-# height = float(input("Enter your height in m: "))
-# weight = float(input("Enter your weight in g : "))
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in g : "))
 
-# BMI = weight /height*2
+BMI = weight /height*2
 
-# print(BMI)
-# if BMI < 18.5:
-#     print(f"Your BMI is {BMI} ,You are underweight")
-# elif BMI <= 25:
-#     print(f"Your BMI is {BMI} ,You are normal weight")
-# elif BMI <= 30:
-#     print(f"Your BMI is {BMI} ,You are overweight")
-# elif BMI <= 35:
-#     print(f"Your BMI is {BMI} ,You are obese")
-# elif BMI > 35:
-#     print(f"Your BMI is {BMI} ,You are underweight")
+print(BMI)
+if BMI < 18.5:
+    print(f"Your BMI is {BMI} ,You are underweight")
+elif BMI <= 25:
+    print(f"Your BMI is {BMI} ,You are normal weight")
+elif BMI <= 30:
+    print(f"Your BMI is {BMI} ,You are overweight")
+elif BMI <= 35:
+    print(f"Your BMI is {BMI} ,You are obese")
+elif BMI > 35:
+    print(f"Your BMI is {BMI} ,You are underweight")
 
-# #Third Challenge
-# #Leap Year
-# #Every year has 365 days but leap year have 366 days one extra in february
-# #year/4 = evenly divisible,year/400 = evenly divisible ,for leap year
-# #year/100 = evenly divisible for normal year
-# year = int(input("Which year do youw want to check? "))
-# year1 = year % 4 == 0 & year % 100 != 0
+#Third Challenge
+#Leap Year
+#Every year has 365 days but leap year have 366 days one extra in february
+#year/4 = evenly divisible,year/400 = evenly divisible ,for leap year
+#year/100 = evenly divisible for normal year
+year = int(input("Which year do youw want to check? "))
+year1 = year % 4 == 0 & year % 100 != 0
 
-# if year % 4 == 0 :
-#     if year % 100 != 0 :
-#         print(f"{year} is leap year")
-#     else:
-#         if year % 400 == 0 :
-#             print(f"{year} is leap year")
-#         else:
-#             print(f"{year} is not leap year")
-# else:
-#     print(f"{year} is not leap year")
+if year % 4 == 0 :
+    if year % 100 != 0 :
+        print(f"{year} is leap year")
+    else:
+        if year % 400 == 0 :
+            print(f"{year} is leap year")
+        else:
+            print(f"{year} is not leap year")
+else:
+    print(f"{year} is not leap year")
     
 #Multiple if statements 
 
-# #Fourth Challenge
-# #Pizza Order
-# print("welcome to Python Pizza Deliveries!")
-# size = input("What size pizza do you want ? S, M, or L ? ")
-# add_Pepperoni = input("Do you want pepperoni? Y or N ? ")
-# extra_cheese = input("Do you want extra cheese? Y or N ? ")
-# bill = 0
-# if size == "S" :
-#     bill = 15
-#     if add_Pepperoni =="Y" :
-#              bill = bill + 2
-# elif size == "M":
-#     bill = 20
-#     if add_Pepperoni =="Y" :
-#              bill = bill + 3
-# elif size == "L":
-#     bill = 25
-#     if add_Pepperoni =="Y" :
-#              bill = bill + 3
 
-# if extra_cheese == "Y" :
-#     bill = bill + 1
-# print(f"Your final bill is : {bill}")
+#Fourth Challenge
+#Pizza Order
+print("welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want ? S, M, or L ? ")
+add_Pepperoni = input("Do you want pepperoni? Y or N ? ")
+extra_cheese = input("Do you want extra cheese? Y or N ? ")
+bill = 0
+if size == "S" :
+    bill = 15
+    if add_Pepperoni =="Y" :
+             bill = bill + 2
+elif size == "M":
+    bill = 20
+    if add_Pepperoni =="Y" :
+             bill = bill + 3
+elif size == "L":
+    bill = 25
+    if add_Pepperoni =="Y" :
+             bill = bill + 3
+
+if extra_cheese == "Y" :
+    bill = bill + 1
+print(f"Your final bill is : {bill}")
 
 #Logical Operators
 
-# #Fifth Challenge
-# #Love Calculator
-# print("Welcome to the Love Calculator!")
-# name1 = input("What is your name? \n")
-# name2 = input("What is their name? \n")
+#Fifth Challenge
+#Love Calculator
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
 
-# name1_lower= name1.lower()
-# name2_lower= name2.lower()
+name1_lower= name1.lower()
+name2_lower= name2.lower()
 
-# t = name1_lower.count("t") + name2_lower.count("t")
-# r = name1_lower.count("r") + name2_lower.count("r")
-# u = name1_lower.count("u") + name2_lower.count("u")
-# e = name1_lower.count("e") + name2_lower.count("e")
+t = name1_lower.count("t") + name2_lower.count("t")
+r = name1_lower.count("r") + name2_lower.count("r")
+u = name1_lower.count("u") + name2_lower.count("u")
+e = name1_lower.count("e") + name2_lower.count("e")
 
-# first_digit =  t + r + u + e
+first_digit =  t + r + u + e
 
-# l = name1_lower.count("l") + name2_lower.count("l")
-# o = name1_lower.count("o") + name2_lower.count("o")
-# v = name1_lower.count("v") + name2_lower.count("v")
-# e = name1_lower.count("e") + name2_lower.count("e")
+l = name1_lower.count("l") + name2_lower.count("l")
+o = name1_lower.count("o") + name2_lower.count("o")
+v = name1_lower.count("v") + name2_lower.count("v")
+e = name1_lower.count("e") + name2_lower.count("e")
 
-# second_digit =  l + o + v + e
-# total_str = str(first_digit)+str(second_digit)
-# total =int(total_str)
-# if total < 10 or total > 90:
-#     print(f"Your Score is {total} % ,You go together like coke and mentos.")
-# elif total > 40 and total < 50:
-#     print(f"Your Score is {total} % ,You are alright together .")
-# else:
-#     print(f"Your Score is {total} % ")
+second_digit =  l + o + v + e
+total_str = str(first_digit)+str(second_digit)
+total =int(total_str)
+if total < 10 or total > 90:
+    print(f"Your Score is {total} % ,You go together like coke and mentos.")
+elif total > 40 and total < 50:
+    print(f"Your Score is {total} % ,You are alright together .")
+else:
+    print(f"Your Score is {total} % ")
 
-# #Final Project
-# #Treasure Island
+#Final Project
+#Treasure Island
 print('''
 *******************************************************************************
           |                   |                  |                     |
