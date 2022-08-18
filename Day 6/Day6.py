b56bfdf5f7123dc6acca53639ab9e234fe3380b8
Ibrahim <m.ibrahimnasir2020@gmail.com>
2022-08-19 00:50:16 +0500

    Day 6 Function and Reeborg's World

--- Day 6/Day6.py
+++ Day 6/Day6.py
@@ -1,44 +1,46 @@
 #Function
-# def my_function():
-#     print("hello world")
+def my_function():
+    print("hello world")
 
-# my_function()
+my_function()
 
-# def max():
-def turn_around():
-    turn_left()
-    turn_left()
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+#Work on this link  and complete different challenges like tuning around or like make a letter I
+
+# def turn_around():
+#     turn_left()
+#     turn_left()
     
-def turn_right():
-    turn_around()
-    turn_left()
+# def turn_right():
+#     turn_around()
+#     turn_left()
     
-def turn_move():
-    move()
-    move()
-    move()
-    move()
+# def turn_move():
+#     move()
+#     move()
+#     move()
+#     move()
     
     
-def turn_ibrahim():
-    turn_move()
-    turn_move()
-    turn_around()
-    turn_move()
-    turn_right()
-    turn_move()
-    turn_move()
-    turn_left()
-    turn_move()
-    turn_around()
-    turn_move()
-    turn_move()
+# def turn_ibrahim():
+#     turn_move()
+#     turn_move()
+#     turn_around()
+#     turn_move()
+#     turn_right()
+#     turn_move()
+#     turn_move()
+#     turn_left()
+#     turn_move()
+#     turn_around()
+#     turn_move()
+#     turn_move()
     
-turn_ibrahim()
+# turn_ibrahim()
     
     
 
-      
+
     
 
 
