321251a8cd4a69c6176a31da61bd4e350c3b1c8d
Ibrahim <m.ibrahimnasir2020@gmail.com>
2022-08-18 17:42:11 +0500

    Day 6 function and karel

---
+++ Day 6/Day6.py
@@ -0,0 +1,52 @@
+#Function
+# def my_function():
+#     print("hello world")
+
+# my_function()
+
+# def max():
+def turn_around():
+    turn_left()
+    turn_left()
+    
+def turn_right():
+    turn_around()
+    turn_left()
+    
+def turn_move():
+    move()
+    move()
+    move()
+    move()
+    
+    
+def turn_ibrahim():
+    turn_move()
+    turn_move()
+    turn_around()
+    turn_move()
+    turn_right()
+    turn_move()
+    turn_move()
+    turn_left()
+    turn_move()
+    turn_around()
+    turn_move()
+    turn_move()
+    
+turn_ibrahim()
+    
+    
+
+      
+    
+
+
+
+
+
+
+
+
+# First Challenge
+#Average Height Exercise
