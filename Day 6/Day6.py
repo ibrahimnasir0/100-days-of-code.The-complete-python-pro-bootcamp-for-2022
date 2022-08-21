92a4240e20453bacef7bdf5f369ec4a87c34a72e
Ibrahim <m.ibrahimnasir2020@gmail.com>
2022-08-21 15:19:03 +0500

    Day 7 Hangman Game

--- Day 6/Day6.py
+++ Day 6/Day6.py
@@ -1,4 +1,7 @@
 #Function
+from platform import java_ver
+
+
 def my_function():
     print("hello world")
 
@@ -37,18 +40,146 @@ my_function()
 #     turn_move()
     
 # turn_ibrahim()
-    
-    
-
-
-    
-
-
-
-
-
-
-
 
 # First Challenge
-#Average Height Exercise
+#Hurdle challenge
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# def turn_around():
+#     turn_left()
+#     turn_left()
+    
+# def turn_right():
+#     turn_around()
+#     turn_left()
+    
+# def turn_jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# for step in range(6):
+#      turn_jump()
+
+#while loop
+
+# Second Challenge
+#Hurdle 2 challenge
+
+
+# def turn_around():
+
+#        turn_left()
+#        turn_left()
+# def turn_right():
+#      turn_around()
+#      turn_left()   
+# def turn_jump():
+#       move()
+#       turn_left()
+#       move()
+#       turn_right()
+#       move()
+#       turn_right()
+#       move()
+#       turn_left()   
+# while at_goal() != True :
+#     turn_jump()
+    
+
+# Third Challenge
+#Hurdle 3 challenge
+
+# def turn_around():
+#     turn_left()
+#     turn_left()
+    
+# def turn_right():
+#     turn_around()
+#     turn_left()
+    
+# def turn_jump():
+#         turn_left()
+#         move()
+#         turn_right()
+#         move()
+#         turn_right()
+#         move()
+#         turn_left()
+   
+# while at_goal() != True :
+    
+#       if wall_in_front():
+#              turn_jump()
+#       else:
+#              move()
+     
+
+    
+# Fourth Challenge
+#Hurdle 4 challenge
+
+
+# def turn_right():
+
+#     turn_left()
+    
+#     turn_left()
+    
+#     turn_left()
+    
+# def jump():
+      
+#       turn_left()
+      
+#       while wall_on_right():
+           
+#            move()
+      
+#       turn_right()
+      
+#       move()
+      
+#       turn_right()
+      
+#       while front_is_clear():
+           
+#            move()
+      
+#       turn_left()
+  
+# while at_goal() != True :
+      
+#       if wall_in_front():
+               
+#                jump()
+      
+#       else:
+               
+#                move()
+
+
+
+    
+
+##final project 
+##Escaping Maze
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# while  not at_goal() :
+#     if right_is_clear():
+#             turn_right()
+#             move()
+#     elif front_is_clear():
+#             move()
+#     else:
+#             turn_left() 
+
