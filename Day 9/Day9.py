5dffab7efceb9ccf53ae948b4f785d0b76a15316
Ibrahim <m.ibrahimnasir2020@gmail.com>
2022-08-22 18:13:43 +0500

    Day 9 Secret Auction

--- Day 9/Day9.py
+++ Day 9/Day9.py
@@ -67,37 +67,70 @@
 # add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
 # print(travel_log)
 
-#Final Project
-#Secret Auction
-from operator import truediv
-import os
-from art9 import logo
+# # Final Project
+# # Secret Auction
+# from operator import truediv
+# import os
+# from art9 import logo
 
+# print(logo)
+
+# bids = {}
+# bidding_finished = False
+
+# def find_highest_bidder(bidding_record):
+#   highest_bid = 0
+#   winner = ""
+#   # bidding_record = {"Angela": 123, "James": 321}
+#   for bidder in bidding_record:
+#     bid_amount = bidding_record[bidder]
+#     if bid_amount > highest_bid: 
+#       highest_bid = bid_amount
+#       winner = bidder
+#   print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+# while not bidding_finished:
+#   name = input("What is your name?: ")
+#   price = int(input("What is your bid?: $"))
+#   bids[name] = price
+#   should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+#   if should_continue == "no":
+#     bidding_finished = True
+#     find_highest_bidder(bids)
+#   elif should_continue == "yes":
+#     os.system('cls')
+    
+from art9 import logo
 print(logo)
 
-bids = {}
-bidding_finished = False
+import os
+bid ={}
+answer =False
+def highestbid(bidding_record):
+    winner =""
+    highest_bid = 0
+    for bidder in bidding_record:
+        bid_amount =bidding_record[bidder]
+        if bid_amount > highest_bid:
+          highest_bid = bid_amount
+          winner = bidder
+    print(f"The winner is {winner} and price is {highest_bid}.")
+while not answer:
+  name = input("What is your name. \n")
+  amount =int(input("Enter your bid ammount. \n"))
+  bid[name] = amount
+  
+  should_continue =input("Do you wanted to continue ?\n")
+  if should_continue =="yes":
+     os.system('cls')
+  elif should_continue =="no":
+      highestbid(bid)
+      answer =True
+      
+    
 
-def find_highest_bidder(bidding_record):
-  highest_bid = 0
-  winner = ""
-  # bidding_record = {"Angela": 123, "James": 321}
-  for bidder in bidding_record:
-    bid_amount = bidding_record[bidder]
-    if bid_amount > highest_bid: 
-      highest_bid = bid_amount
-      winner = bidder
-  print(f"The winner is {winner} with a bid of ${highest_bid}")
 
-while not bidding_finished:
-  name = input("What is your name?: ")
-  price = int(input("What is your bid?: $"))
-  bids[name] = price
-  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
-  if should_continue == "no":
-    bidding_finished = True
-    find_highest_bidder(bids)
-  elif should_continue == "yes":
-    os.system('cls')
+    
+
 
 
