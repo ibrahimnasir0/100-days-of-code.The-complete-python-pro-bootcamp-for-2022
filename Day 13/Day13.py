eb56a55f04b3dfefdce14b5dc904d92bfc72ee5e
Ibrahim <m.ibrahimnasir2020@gmail.com>
2022-08-28 14:58:57 +0500

    Day 13 uncomment files

--- Day 13/Day13.py
+++ Day 13/Day13.py
@@ -1,73 +1,72 @@
-############DEBUGGING#####################
+###########DEBUGGING#####################
+# Describe Problem
+def my_function():
+  for i in range(1, 21):
+    if i == 20:
+      print("You got it")
+my_function()
 
-# # Describe Problem
-# def my_function():
-#   for i in range(1, 21):
-#     if i == 20:
-#       print("You got it")
-# my_function()
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0,5 )
+print(dice_imgs[dice_num])
 
-# # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(0,5 )
-# print(dice_imgs[dice_num])
+# Play Computer
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year >= 1994:
+  print("You are a Gen Z.")
 
-# # Play Computer
-# year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
-#   print("You are a millenial.")
-# elif year >= 1994:
-#   print("You are a Gen Z.")
+# Fix the Errors
+age = int(input("How old are you?"))
+if age > 18:
+   print(f"You can drive at age {age}.")
 
-# # Fix the Errors
-# age = int(input("How old are you?"))
-# if age > 18:
-#    print(f"You can drive at age {age}.")
+#Print is Your Friend
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
 
-# #Print is Your Friend
-# pages = 0
-# word_per_page = 0
-# pages = int(input("Number of pages: "))
-# word_per_page = int(input("Number of words per page: "))
-# total_words = pages * word_per_page
-# print(total_words)
+#Use a Debugger
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
 
-# #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#     b_list.append(new_item)
-#   print(b_list)
-
-# mutate([1,2,3,5,8,13])
-
-
-
-# #First Debug Challenge
-# number = int(input("Which number do you want to check?"))
-
-# if number % 2 == 0:
-#   print("This is an even number.")
-# else:
-#   print("This is an odd number.")
+mutate([1,2,3,5,8,13])
 
 
 
-# #Second Debug Challenge 
-# year = int(input("Which year do you want to check?"))
+#First Debug Challenge
+number = int(input("Which number do you want to check?"))
 
-# if year % 4 == 0:
-#   if year % 100 == 0:
-#     if year % 400 == 0:
-#        print("Leap year.")
-#     else:
-#        print("Not leap year.")
-#   else:
-#        print("Leap year.")
-# else:
-#   print("Not leap year.")
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
+
+
+#Second Debug Challenge 
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+       print("Leap year.")
+    else:
+       print("Not leap year.")
+  else:
+       print("Leap year.")
+else:
+  print("Not leap year.")
 
 #Third Debug Challenge 
 for number in range(1, 101):
